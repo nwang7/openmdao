8fd6bc5500a736475443b90ebaa2f2fb6ed7e813
nwang7 <nathan.wang@gatech.edu>
2020-03-05 18:53:53 -0500

    intial commit

---
+++ Practice/parabloid_min.py
@@ -0,0 +1,28 @@
+import openmdao.api as om
+
+# build the model
+prob = om.Problem()
+indeps = prob.model.add_subsystem('indeps', om.IndepVarComp())
+indeps.add_output('x', 3.0)
+indeps.add_output('y', -4.0)
+
+prob.model.add_subsystem('paraboloid', om.ExecComp('f = (x-3)**2 + x*y + (y+4)**2 - 3'))
+
+prob.model.connect('indeps.x', 'paraboloid.x')
+prob.model.connect('indeps.y', 'paraboloid.y')
+
+prob.driver = om.ScipyOptimizeDriver()
+prob.driver.options['optimizer'] = 'SLSQP'
+
+prob.model.add_design_var('indeps.x', lower=-50, upper=50)
+prob.model.add_design_var('indeps.y', lower=-50, upper=50)
+prob.model.add_objective('paraboloid.f')
+
+prob.setup()
+prob.run_driver()
+
+# minimum value
+print('min = ', prob['paraboloid.f'])
+# location of the minimum
+print('  x = ', prob['indeps.x'])
+print('  y = ', prob['indeps.y'])
